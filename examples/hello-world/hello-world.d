6fce68cdbbc5947bae4dadb95835e2fa34e8eb1c
fistw <fuliyu1225@gmail.com>
2018-12-13 17:25:30 +0800

    basic version

---
+++ examples/hello-world/hello-world.d
@@ -0,0 +1,27 @@
+hello-world.co: hello-world.c ../../core/./contiki.h \
+ ../../core/./contiki-version.h ../../platform/evb1000/./contiki-conf.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h ../../core/./contiki-default-conf.h \
+ ../../core/./sys/process.h ../../core/./sys/pt.h ../../core/./sys/lc.h \
+ ../../core/./sys/lc-switch.h ../../core/./sys/cc.h \
+ ../../core/./sys/cc-gcc.h ../../core/./sys/autostart.h \
+ ../../core/./sys/process.h ../../core/./sys/timer.h \
+ ../../core/./sys/clock.h ../../core/./sys/ctimer.h \
+ ../../core/./sys/etimer.h ../../core/./sys/timer.h \
+ ../../core/./sys/etimer.h ../../core/./sys/rtimer.h \
+ ../../cpu/stm32f105/./rtimer-arch.h ../../core/./sys/rtimer.h \
+ ../../core/./sys/pt.h ../../core/./sys/procinit.h \
+ ../../core/./sys/loader.h ../../core/./sys/clock.h \
+ ../../core/./sys/energest.h /usr/include/newlib/stdio.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h
---
+++ examples/ipv6/simple-udp-rpl/broadcast-example.d
@@ -0,0 +1,42 @@
+broadcast-example.co: broadcast-example.c ../../../core/./contiki.h \
+ ../../../core/./contiki-version.h \
+ ../../../platform/evb1000/./contiki-conf.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_stdint.h ../../../core/./contiki-default-conf.h \
+ ../../../core/./sys/process.h ../../../core/./sys/pt.h \
+ ../../../core/./sys/lc.h ../../../core/./sys/lc-switch.h \
+ ../../../core/./sys/cc.h ../../../core/./sys/cc-gcc.h \
+ ../../../core/./sys/autostart.h ../../../core/./sys/process.h \
+ ../../../core/./sys/timer.h ../../../core/./sys/clock.h \
+ ../../../core/./sys/ctimer.h ../../../core/./sys/etimer.h \
+ ../../../core/./sys/timer.h ../../../core/./sys/etimer.h \
+ ../../../core/./sys/rtimer.h ../../../cpu/stm32f105/./rtimer-arch.h \
+ ../../../core/./sys/rtimer.h ../../../core/./sys/pt.h \
+ ../../../core/./sys/procinit.h ../../../core/./sys/loader.h \
+ ../../../core/./sys/clock.h ../../../core/./sys/energest.h \
+ ../../../core/./lib/random.h ../../../core/./sys/ctimer.h \
+ ../../../core/./net/ip/uip.h ../../../core/./net/ip/uipopt.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h ../../../core/./net/ip/tcpip.h \
+ ../../../core/./net/ipv6/uip-ds6.h ../../../core/./sys/stimer.h \
+ ../../../core/./net/ipv6/uip-nd6.h \
+ ../../../core/./net/ipv6/uip-ds6-route.h ../../../core/./net/nbr-table.h \
+ ../../../core/./net/linkaddr.h ../../../core/./net/netstack.h \
+ ../../../core/./net/llsec/llsec.h ../../../core/./net/mac/mac.h \
+ ../../../core/./dev/radio.h ../../../core/./net/mac/rdc.h \
+ ../../../core/./net/llsec/llsec802154.h \
+ ../../../core/./net/mac/frame802154.h ../../../core/./net/mac/framer.h \
+ ../../../core/./lib/list.h ../../../core/./net/ipv6/uip-ds6-nbr.h \
+ ../../../core/net/ip/simple-udp.h /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/machine/types.h \
+ /usr/include/newlib/sys/stdio.h
