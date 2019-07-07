d5b418f3959a16620a1429d02fbd370fc630be81
chenxin <nihao_cx@163.com>
2019-07-07 10:18:22 +0800

    modify RF config to 110K

--- dev/dw1000/project_common_conf.h
+++ dev/dw1000/project_common_conf.h
@@ -2,16 +2,17 @@
 #define PROJECT_COMMON_CONF_H_
 
 // 射频基本设置
-#define chan_CONFIG             5
-#define prf_CONFIG              DWT_PRF_16M
-#define txPreambLength_CONFIG   DWT_PLEN_128
-#define dataRate_CONFIG         DWT_BR_6M8
-#define txCode_CONFIG           7
-#define rxCode_CONFIG           7
-#define rxPAC_CONFIG            DWT_PAC8
+#define chan_CONFIG             4
+#define prf_CONFIG              DWT_PRF_64M
+#define txPreambLength_CONFIG   DWT_PLEN_2048
+#define dataRate_CONFIG         DWT_BR_110K
+#define txCode_CONFIG           17
+#define rxCode_CONFIG           17
+#define rxPAC_CONFIG            DWT_PAC64
 #define nsSFD_CONFIG            0  
 #define phrMode_CONFIG          DWT_PHRMODE_STD
-#define sfdTO_CONFIG            (129 + 8 - 8)
+// sfdTO=preamble length + 1 + SFD length - PAC size 
+#define sfdTO_CONFIG            (2049 + 8 - 64)
 
 // UWB标签类型（当编译UWB基站时，注释此预定义）
 #define UWB_TYPE_TAG_CONFIG
--- examples/tdoa3_ranging/anchor_conf.h
+++ examples/tdoa3_ranging/anchor_conf.h
@@ -3,7 +3,8 @@
 
 #include "project_common_conf.h"
 
-// 发射功率（默认值=33.5db）
+// tx配置（默认信道4，发射功率33.5db）
+#define txPGdly_CONFIG              0xC0
 #define txPower_CONFIG              0x1F1F1F1F
 // 基站ID
 #define anchorID_CONFIG             0x0a
